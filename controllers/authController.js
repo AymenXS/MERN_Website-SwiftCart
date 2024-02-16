e22447f16474e54e30b699e1a77ca10488bfe58e
Aymen <aymenghaloua@gmail.com>
2024-02-16 14:17:03 +0100

    Adding and Refactoring JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,10 @@ const {
   BadRequestError,
 } = require('../errors');
 
+const { createJWT } = require('../utils');
+
 const register = async (req, res) => {
+  // {...} From the front-end endpoint.
   const { email, name, password } = req.body;
 
   const emailAlreadyExists = await User.findOne({ email });
@@ -18,9 +21,20 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
+  // {...} From the User Schema; MongoDB.
   const user = await User.create({ email, name, password, role });
-  res.status(StatusCodes.CREATED).json({ user });
-  res.send('register user');
+
+  const tokenUser = { name: user.name, userID: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
