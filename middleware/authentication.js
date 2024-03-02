7dc4919be92a22f57873b6217b4011d2f9119868
Aymen <aymenghaloua@gmail.com>
2024-03-02 05:30:00 +0100

    Updating

---
+++ middleware/authentication.js
@@ -0,0 +1,17 @@
+const CustomErr = require('../errors');
+const { isTokenValid } = require('../utils');
+
+const authenticateUser = async(req, res, next)=>{
+  const token = req.signedCookies.token
+
+  if(!token){
+    console.log('error, no token present');
+  }
+
+  console.log('token present');
+
+}
+
+module.exports = {
+  authenticateUser
+}
