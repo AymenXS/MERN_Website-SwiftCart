57fe8aabfa6abcf5d93ef05e74bdae04ec9a0c7c
Aymen <aymenghaloua@gmail.com>
2024-03-02 06:27:14 +0100

    Adding "Admin" access permission

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,16 +1,31 @@
-const CustomErr = require('../errors');
+const CustomAPIError = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
+
   if (!token) {
-    console.log('error, no token present');
-  } else {
-    console.log('token present');
+    throw new CustomAPIError.UnauthenticatedError('Token missing, Authentication Invalid');
   }
+
+  try {
+    const { name, userID, role } = isTokenValid({ token });
+    req.user = { name, userID, role };
+    next();
+  } catch (error) {
+    throw new CustomAPIError.UnauthenticatedError('Authentication Invalid');
+  }
+};
+
+const authorizePermissions = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    throw new CustomAPIError.UnauthenticatedError('Unauthorized to access this route');
+  }
+  
   next();
 };
 
 module.exports = {
   authenticateUser,
+  authorizePermissions,
 };
