402d92b0b53b4ee5d87029712cc6e33d070f24eb
Aymen <aymenghaloua@gmail.com>
2024-03-07 22:58:25 +0100

    Adding "UpdatePassword" Functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
 const getAllUsers = async (req, res) => {
-  console.log(req.user);
   const users = await User.find({ role: 'user' }).select('-password');
   res.status(StatusCodes.OK).json({ users });
 };
@@ -26,7 +25,22 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-  res.send(req.body);
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError('Please provide both values');
+  }
+
+  const user = await User.findOne({ _id: req.user.userID });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+
+  user.password = newPassword;
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated' });
 };
 
 module.exports = {
