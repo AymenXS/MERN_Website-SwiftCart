fc1bef857e87feb2046b99bde119b408182c7cfd
Aymen <aymenghaloua@gmail.com>
2024-03-08 21:26:02 +0100

    Adding "Product Routes"

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const cookieParser = require('cookie-parser');
 // Routers
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
 
 // Middlewares
 const notFoundMiddleware = require('./middleware/not-found');
@@ -33,6 +34,8 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 const {
+  getAllProducts,
   createProduct,
   getSingleProduct,
   updateProduct,
   deleteProduct,
   uploadImage,
 } = require('../controllers/productController');
+const { authorizePermissions, authenticateUser } = require('../middleware/authentication');
+
+router
+  .route('/')
+  .get(getAllProducts)
+  .post([authenticateUser, authorizePermissions('admin')], createProduct);
+
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
+  .delete([authenticateUser, authorizePermissions('admin')], deleteProduct);
+
+router.route('/uploadImage').post([authenticateUser, authorizePermissions('admin')], uploadImage);
 
 module.exports = router;
