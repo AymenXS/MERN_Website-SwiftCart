88075b234904ddb327c5b8eeb33f64ffdf54a93b
Aymen <aymenghaloua@gmail.com>
2024-03-16 22:28:31 +0000

    Adding "Order" basic skeleton.

---
+++ controllers/orderController.js
@@ -0,0 +1,23 @@
+const getAllOrders = async (req, res) => {
+  res.send('create order');
+};
+const getSingleOrder = async (req, res) => {
+  res.send('create order');
+};
+const createOrder = async (req, res) => {
+  res.send('create order');
+};
+const getCurrentUserOrders = async (req, res) => {
+  res.send('create order');
+};
+const updateOrder = async (req, res) => {
+  res.send('create order');
+};
+
+module.exports = {
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
+};
